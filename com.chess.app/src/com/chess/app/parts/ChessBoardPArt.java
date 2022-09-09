95fa4c53d4ae7b2bda08137649b6e33f9b35f377
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2022-09-09 00:04:02 -0300

    Projeto criado

---
+++ com.chess.app/src/com/chess/app/parts/ChessBoardPArt.java
@@ -0,0 +1,26 @@
+package com.chess.app.parts;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+public class ChessBoardPArt {
+
+	private Label label;
+
+	@PostConstruct
+	public void createComposite(Composite parent) {
+		parent.setLayout(new GridLayout(1, false));
+
+		label = new Label(parent, SWT.BORDER | SWT.BOLD);
+		label.setText("Hello!");
+	}
+
+	@Focus
+	public void setFocus() {
+	}
+}
